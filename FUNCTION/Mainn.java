fe217cec3a04eac168926f6bb416a075d5c9cd82
Akshay Umredkar <akshayumredkar9822@gmail.com>
2023-11-13 12:42:32 +0530

    function

---
+++ FUNCTION/Mainn.java
@@ -0,0 +1,15 @@
+public class Mainn {
+    public static void main(String[] args) {
+
+        //String Function
+        String name="Akshay";
+        changeName(name);
+        System.out.println(name);
+    }
+
+    private static void changeName(String naam) {
+        naam="Suppu";   //creating a new object
+                        //u can not modified string
+
+    }
+}
